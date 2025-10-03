7c3dd6ce39f6ff27330832aa0302f2ffcd2cedf7
JormanGuadarrama <jguadarrama@csumb.edu>
2025-10-02 23:36:06 -0700

    Reader and ReaderTest completed and test pasts

---
+++ src/main/java/Reader.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * @author guada
+ * @version 0.1.0
+ * @since 10/2/2025
+ **/
+public class Reader {
+    public static final int CARD_NUMBER_ = 0;
+    public static final int NAME_ = 1;
+    public static final int PHONE_ = 2;
+    public static final int BOOK_COUNT_ = 3;
+    public static final int BOOK_START_ = 4;
+
+    private int cardNumber;
+    private String name;
+    private String phone;
+    private List<Book> books;
+
+    public Reader(int cardNumber, String name, String phone) {
+        this.cardNumber = cardNumber;
+        this.name = name;
+        this.phone = phone;
+        this.books = new ArrayList<>();
+    }
+
+    public Code addBook(Book book) {
+        if (hasBook(book)) {
+            return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
+        }
+        books.add(book);
+        return Code.SUCCESS;
+    }
+
+    public Code removeBook(Book book) {
+        if (!hasBook(book)) {
+            return Code.READER_DOESNT_HAVE_BOOK_ERROR;
+        }
+        boolean removed = books.remove(book);
+        return removed ? Code.SUCCESS : Code.READER_COULD_NOT_REMOVE_BOOK_ERROR;
+    }
+
+    public boolean hasBook(Book book) {
+        return books.contains(book);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reader)) return false;
+        Reader reader = (Reader) o;
+        return cardNumber == reader.cardNumber && Objects.equals(name, reader.name) && Objects.equals(phone, reader.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardNumber, name, phone);
+    }
+
+    @Override
+    public String toString() {
+        String titles = books.stream().map(Book::getTitle).collect(Collectors.joining(", "));
+        return String.format("%s (#%d) has checked out {%s}", name, cardNumber, titles);
+    }
+
+    public int getBookCount() {
+        return books.size();
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+}
